83a0bbeb4a5c51f99386e11e77978a382a762ce0
Leigh-AnnVanBelling <leighannvanbelling@gmail.com>
2020-06-13 19:33:21 +0200

    Initial Commit

---
+++ The Destini/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Destini
+//
+//  Created by Leigh-Ann Van Belling on 2020/06/13.
+//  Copyright © 2020 Leigh-Ann Van Belling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
